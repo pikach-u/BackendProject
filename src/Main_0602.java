35e6e5cf0eea50d64202aae80f22d0b526c905c6
pikach-u <ziwon05@naver.com>
2025-06-02 10:28:15 +0900

    0602 Operator

--- src/Main_0602.java
+++ src/Main_0602.java
@@ -1,12 +1,25 @@
-//0602 constant-variable
+//0602 Operator
 
 public class Main_0602 {
     public static void main(String[] args) {
 
-        final int a = 10;
-        System.out.println(a);
-        a = 11;     // error: cannot assign a value to final variable
-        System.out.println(a);
+        int a = 10;
+        int b = 3;
+
+        int sum = a+b;
+        System.out.println(sum);
+
+        int difference = a-b;
+        System.out.println(difference);
+
+        int product = a*b;
+        System.out.println(product);
+
+        int quotient = a/b;
+        System.out.println(quotient);
+
+        int remainder = a%b;
+        System.out.println(remainder);
 
     }
 }
