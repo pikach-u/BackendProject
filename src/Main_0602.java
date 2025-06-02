81c799031b59992c35e728fd53339bb782fc2cd2
pikach-u <ziwon05@naver.com>
2025-06-02 14:45:36 +0900

    0602 Switch

--- src/Main_0602.java
+++ src/Main_0602.java
@@ -1,38 +1,24 @@
-//0602 Conditional-Statement-If
+//0602 Switch
 
 public class Main_0602 {
     public static void main(String[] args) {
-        // if-else
-        int num = -1;
+        // "happy", "angry", "sad", etc.
+        String mood = "angry";
 
-        if(num > 0) {
-            System.out.println("number는 0보다 큽니다");
+        switch(mood){
+            case "happy":
+                System.out.println("행복합니다");
+                break;      //여러 case를 묶어서 처리하는 경우는 break 생략
+            case "tired":
+                System.out.println("피곤합니다");
+                break;
+            case "angry":
+                System.out.println("화납니다");
+                break;
+            default:
+                System.out.println("표현할 수 없습니다");
+
         }
-        else{
-            System.out.println("number는 0이거나 0보다 작습니다.");
-        }
-
-        //if-else if-else
-        num = 5;
-
-        if(num > 0) {
-            System.out.println("number는 0보다 큽니다");
-        } else if (num < 0) {
-            System.out.println("number는 0보다 작습니다");
-        } else{
-            System.out.println("number는 0보다 같습니다");
-        }
-
-
-        //if-else if
-        num = -1;
-        if (num > 0) {
-            System.out.println("number는 양수입니다.");
-        }
-        else if (num < 0) {
-            System.out.println("number는 음수입니다.");
-        }
-
 
     }
 }
