0414e18bf6fd6f4d814c76f79f2dbc0841ebe6c2
pikach-u <ziwon05@naver.com>
2025-06-02 09:40:47 +0900

    0602 final variable

--- src/Main_0602.java
+++ src/Main_0602.java
@@ -1,5 +1,12 @@
+//0602 constant-variable
+
 public class Main_0602 {
     public static void main(String[] args) {
+
+        final int a = 10;
+        System.out.println(a);
+        a = 11;     // error: cannot assign a value to final variable
+        System.out.println(a);
 
     }
 }
