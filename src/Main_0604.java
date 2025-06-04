660b48ed80060436c625a0576821fe573be6fb2e
pikach-u <ziwon05@naver.com>
2025-06-04 15:56:10 +0900

    0604 toBinaryString, toHexString

--- src/Main_0604.java
+++ src/Main_0604.java
@@ -1,45 +1,14 @@
 //0604 While
 
-import java.sql.SQLOutput;
-
 public class Main_0604 {
     public static void main(String[] args) {
-        int i = 0;
-        while (i < 5) {
-            System.out.println(i);
-            i++;
-        }
+        int num = 12144523;
 
-        boolean a = true;
-        int b = 1;
-        while (a) {
-            System.out.println(b);
-            b++;
+        String binaryString = Integer.toBinaryString(num);  //이진수로 변환
+        String hexaString = Integer.toHexString(num);
 
-            if (b > 10000) {    //조건 만족 시 탈출
-                a = false;
-            }
-        }
-
-        int input = 5;
-        do {
-            System.out.println("자판기입니다");
-
-            switch (input) {
-                case 1:
-                    System.out.println("콜라");
-                    break;
-                case 2:
-                    System.out.println("사이다");
-                    break;
-                case 3:
-                    System.out.println("물");
-                    break;
-                default:
-                    System.out.println("꽝");
-            }
-            input = 0;
-        } while (input != 0);
-
+        System.out.println("Decimal: " + num);
+        System.out.println("Binary: " + binaryString);
+        System.out.println("Hexa: " + hexaString);
     }
 }
