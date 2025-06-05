f55d46d3fadad6b9a30ea1aac3a207c1cf283a0b
pikach-u <ziwon05@naver.com>
2025-06-05 14:15:54 +0900

    0605 Function

--- src/Main_0605.java
+++ src/Main_0605.java
@@ -1,51 +1,16 @@
-//0605 Ternary-Operators
+//0605 Function
 
 public class Main_0605 {
+    public static void printStudent(String name, int age) {
+        System.out.println("이름: " + name);
+        System.out.println("나이: " + age);
+        System.out.println("------------");
+    }
+
     public static void main(String[] args) {
-        int a = 10;
-        int b = 13;
-
-        int max = (a > b) ? a : b;
-        System.out.println(max);
-
-        //if문으로 변환
-//        int max = b;
-//
-//        if (a < b) {
-//            max = b;
-//        } else {
-//            max = a;
-//        }
-//        System.out.println(max);
-
-        int score = 61;
-        String result = (score > 60) ? "통과" : "탈락";
-
-        System.out.println("결과: " + result);
-
-        //if문으로 변환
-//        if (score > 60) {
-//            result = "통과";
-//        } else {
-//            result = "탈락";
-//        }
-//
-//        System.out.println("결과: " + result);
-
-        // 삼항연산자를 중첩해서 쓰는 형태 - 권장X
-        String color = "보라";
-        String choiceColor = (color == "빨강") ? "빨강" : (color == "파랑") ? "파랑" : "보라";
-
-        System.out.println(choiceColor);
-
-        //if문으로 변환 - 중첩보다 if문 추천
-//        if(color =="빨강") {
-//            System.out.println("빨강");
-//        } else if (color == "파랑") {
-//            System.out.println("파랑");
-//        } else{
-//            System.out.println("보라");
-//        }
-
+        System.out.println("=== 학생 정보 출력 ===");
+        Main_0605.printStudent("Alice", 20);
+        printStudent("Bob", 22);    //같은 클래스 내에 있어서 생략가능
+        printStudent("Charlie", 25);
     }
 }
