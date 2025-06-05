a418c4947554ac9a71f71f74d4802ba8a7642cef
pikach-u <ziwon05@naver.com>
2025-06-05 13:17:37 +0900

    0605 Ternary-Operators

--- src/Main_0605.java
+++ src/Main_0605.java
@@ -1,30 +1,51 @@
-//0605 break-continue
+//0605 Ternary-Operators
 
 public class Main_0605 {
     public static void main(String[] args) {
+        int a = 10;
+        int b = 13;
 
-        for (int i = 1; i <= 20; i++) {
-            if (i >= 15) {
-                break;
-            }
+        int max = (a > b) ? a : b;
+        System.out.println(max);
 
-            if (i % 5 == 0) {
-                continue;
-            }
+        //if문으로 변환
+//        int max = b;
+//
+//        if (a < b) {
+//            max = b;
+//        } else {
+//            max = a;
+//        }
+//        System.out.println(max);
 
-            System.out.println(i);
-        }
+        int score = 61;
+        String result = (score > 60) ? "통과" : "탈락";
 
-        for (int i = 1; i <= 1000; i++) {
-            if (i % 2 != 0 || i % 3 != 0) { // 6의 배수 찾기
-                if (i == 100) {
-                    break;
-                }
-                continue;
-            }
+        System.out.println("결과: " + result);
 
-            System.out.println(i);
-        }
+        //if문으로 변환
+//        if (score > 60) {
+//            result = "통과";
+//        } else {
+//            result = "탈락";
+//        }
+//
+//        System.out.println("결과: " + result);
+
+        // 삼항연산자를 중첩해서 쓰는 형태 - 권장X
+        String color = "보라";
+        String choiceColor = (color == "빨강") ? "빨강" : (color == "파랑") ? "파랑" : "보라";
+
+        System.out.println(choiceColor);
+
+        //if문으로 변환 - 중첩보다 if문 추천
+//        if(color =="빨강") {
+//            System.out.println("빨강");
+//        } else if (color == "파랑") {
+//            System.out.println("파랑");
+//        } else{
+//            System.out.println("보라");
+//        }
 
     }
 }
