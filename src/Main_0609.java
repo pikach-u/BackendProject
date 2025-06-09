3695508865f5e88a0ff36241dfcf07163d03f01f
pikach-u <ziwon05@naver.com>
2025-06-09 15:22:40 +0900

    0609 Strings

--- src/Main_0609.java
+++ src/Main_0609.java
@@ -1,30 +1,36 @@
-//0609 Arrays
+//0609 Strings
 
 import java.lang.reflect.Array;
 
 public class Main_0609 {
     public static void main(String[] args) {
 
-        int[] num = {1, 2, 3, 4, 5};
-        for (int i = 0; i < 5; i++) {
-            System.out.println(num[i]);
-        }
-        System.out.println(num.length);
-        for (int i = 0; i < num.length; i++) {
-            System.out.println(num[i]);
-        }
-        for (int i = num.length - 1; i >= 0; i--) {
-            System.out.println(num[i]);
+        String mess = "Hello, Java!";
+        System.out.println(mess.length());
+        System.out.println(mess.toUpperCase());
+        System.out.println(mess.toLowerCase());
+        System.out.println(mess.indexOf("J"));
+
+        for (int i = 0; i < mess.length(); i++) {
+            if(mess.charAt(i) == 'l'){
+                System.out.println("l은 " + (i+1) +"번째입니다");
+
+            }
         }
 
-        num[2] = 99;    //값 수정 가능
+        String address = "0x120819f5573ac3A530e6172b1581613A09b4b338";
+        System.out.println(address.substring(0,7));
+        System.out.println(address.substring(37,42));
+        System.out.println(address.length());
+        System.out.println(
+                address.substring(0,7)
+                + "..."
+                + address.substring(address.length() - 5)
+        );
 
-        int[] numbers = {2, 4, 6, 8, 10};
-        int sum = 0;
-        for (int i = 0; i < numbers.length; i++) {
-            sum += numbers[i];
-        }
-        System.out.println(sum);
+        String greeting = "Hello, Alice!";
+        greeting = greeting.toLowerCase();
+        System.out.println(greeting);
 
     }
 }
