1cdfe67af38eb362b8be96b359e7a82131e1d8fd
pikach-u <ziwon05@naver.com>
2025-06-11 16:00:26 +0900

    0611 계산기만들기 1

--- src/Main_0611.java
+++ src/Main_0611.java
@@ -1,4 +1,4 @@
-//0611 자판기만들기
+//0611 계산기만들기
 
 import java.util.Scanner;
 
@@ -7,65 +7,56 @@ public class Main_0611 {
 
         Scanner scanner = new Scanner(System.in);
 
-        //입력 값에 따른 조건문
-        //if문 (숫자 비교), 스위치 (값만 체크)
+        //상수로 사칙연산 정의
+        final String PLUS = "+";
+        final String MINUS = "-";
+        final String MULTIPLY = "*";
+        final String DIVISION = "/";
 
-        int price = 0;
-        String item = "";
+        double a = 0;    //왼쪽 피연산자
+        double b = 0;    //오른쪽 피연산자
+        String op;          //연산자
+        double answer = 0;  //답
+
+
+        System.out.println("========계산기=========");
+        System.out.println("두 수를 입력해주세요.");
+        System.out.println("======================");
+
+        //계산할 숫자 입력
+        System.out.println("첫 번째 숫자: ");
+        a = scanner.nextDouble();
+
+        System.out.println("두 번째 숫자: ");
+        b = scanner.nextDouble();
+
+        System.out.println("1. + 2. - 3. * 4. /");
 
         while (true) {
 
-            System.out.println("=== 자판기 메뉴 ===");
-            System.out.println("1. Melon Soda (800원)");
-            System.out.println("2. Cider (2110원)");
-            System.out.println("3. Coke (2000원)");
-            System.out.println("4. Juice (3000원)");
-            System.out.println("5. Coffee (5500원)");
-            int choice = scanner.nextInt();
+            op = scanner.next();
 
-            switch (choice) {
-                case 1:
-                    item = "Melon Soda";
-                    price = 800;
+            switch (op) {
+                case PLUS:
+                    answer = a + b;
                     break;
-                case 2:
-                    item = "Cider";
-                    price = 2110;
+                case MINUS:
+                    answer = a - b;
                     break;
-                case 3:
-                    item = "Coke";
-                    price = 2000;
+                case MULTIPLY:
+                    answer = a * b;
                     break;
-                case 4:
-                    item = "Juice";
-                    price = 3000;
-                    break;
-                case 5:
-                    item = "Coffee";
-                    price = 5500;
+                case DIVISION:
+                    answer = a / b;
                     break;
                 default:
-                    System.out.println("다시 선택해주세요");
+                    System.out.println("다시 입력해주세요");
                     continue;
-                    //return; //return시 switch문 밖 로직은 실행안됨
             }
             break;
         }
 
-        System.out.println(item + "을 선택하셨습니다. 가격은 " + price + "원입니다.");
-        int money = scanner.nextInt();
-
-        while (true) {
-            if (money < price) {
-                System.out.println("금액이 부족합니다. 추가 투입해주세요");
-                money += scanner.nextInt();
-
-            } else {
-                int change = money - price; //잔돈
-                System.out.printf("%s의 거래가 완료되었습니다. 잔돈: %d\n", item, change);
-                return;
-            }
-        }
+        System.out.printf("답은 %.2f입니다.\n", answer);
 
     }
 }
