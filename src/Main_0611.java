56eb044b1ce6179d08853b6788ca91657668588e
pikach-u <ziwon05@naver.com>
2025-06-11 13:36:24 +0900

    0611 Scanner

--- src/Main_0611.java
+++ src/Main_0611.java
@@ -1,25 +1,22 @@
-//0611 Print-Methods
+//0611 Scanner
+
+import java.util.Scanner;
 
 public class Main_0611 {
     public static void main(String[] args) {
-        String name = "Kim";
-        int num = 10;
-        double pi = 3.14159;
 
+        Scanner scanner = new Scanner(System.in);
+        // scanner.nextLine();
+        String name = scanner.nextLine();
+        System.out.println(name);
 
-        //타입 확인
-        System.out.println((name + num).getClass().getName());
-        System.out.println(((Object) num).getClass().getName());
+        int age = scanner.nextInt();
+        System.out.println(age);
 
-        System.out.println("\"안녕하세요.\"");
-        System.out.println("이름:\tAlice");
-        System.out.println("\\\\");
+        char thirdChar = scanner.next().charAt(3);
+        System.out.println(thirdChar);
 
-        System.out.printf("나이: %d, 원주율: %.2f\n", num, pi);
-
-        int fineDust = 23;
-        double ozone = 0.051;
-        System.out.printf("미세먼지: %d, 오존: %.3f", fineDust, ozone);
+        System.out.println("프로그램 종료");
 
     }
 }
