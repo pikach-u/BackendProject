5dfd9fd3ffd98d9fa49e9b66340d289e7da93750
pikach-u <ziwon05@naver.com>
2025-06-11 10:45:15 +0900

    0611 Print-Methods-1

--- src/Main_0611.java
+++ src/Main_0611.java
@@ -1,35 +1,12 @@
-//0611 Constructors
-
-class Person1{
-    String name;
-    int age;
-
-    public Person1(String name, int age){
-        this.name = name;
-        this.age = age;
-    }
-}
-
-class Car{
-    String brand;
-    int year;
-
-    public Car(String brand, int year){
-        this.brand = brand;
-        this.year = year;
-    }
-
-    public void displayInfo(){
-        System.out.println("이 차는 " + brand + "이고, 연식은 " + year + "년입니다.");
-    }
-}
+//0611 Print-Methods
 
 public class Main_0611 {
     public static void main(String[] args){
-        Person1 p1 = new Person1("꿀꽈배기", 23);
-        Person1 p2 = new Person1("우사기", 34);
-
-        Car car = new Car("BMW", 23);
-        car.displayInfo();
+        System.out.print("프린트 함수랑");
+        System.out.println("프린트 함수 LN 함수의");
+        System.out.print("차이점을 찾아서");
+        System.out.println("디스코드 채팅으로");
+        System.out.print("알려주시면");
+        System.out.println("좋을것 같습니다.");
     }
 }
