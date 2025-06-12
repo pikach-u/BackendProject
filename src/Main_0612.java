db0be965a63756827ff2e7ed68f5d688949ac937
pikach-u <ziwon05@naver.com>
2025-06-12 11:33:38 +0900

    0612 Override-1

--- src/Main_0612.java
+++ src/Main_0612.java
@@ -1,68 +1,33 @@
-//0612 Inheritance
-
-class Animal{
+//0612 Override
+class Animal {
     String name;
 
-    public Animal(String name){    //생성자
+    public Animal(String name) {
         this.name = name;
     }
 
-    public void printName(){
-        System.out.println("Animal name: " + this.name);
+    public void printName() {
+        System.out.println("name: " + name);
     }
 }
 
-class Dog extends Animal{
+class Dog extends Animal {
     String breed;
 
-    public Dog(String name, String breed){
-        super(name);        //부모 클래스의 객체에 먼저 값을 넣어주어야 함(?)
+    public Dog(String name, String breed) {
+        super(name);
         this.breed = breed;
-        //super.name = name;
     }
 
-}
-
-//class Dog{                //위 코드와 기능이 같음
-//    String name;          //상속의 기능: 중복코드 제거 효과
-//}
-
-class Vehicle{
-    String brand;
-
-    public Vehicle(String brand){
-        this.brand = brand;
-    }
-
-    public void display(){
-        System.out.println("brand: " + this.brand);
+    @Override
+    public void printName() {
+        System.out.println("name: " + name + " breed: " + breed);
     }
 }
-
-class Car extends Vehicle{
-    String model;
-
-    public Car(String brand, String model){
-        super(brand);
-        this.model = model;
-    }
-    public void display(){
-        System.out.println("brand: " + this.brand + " " + this.model);
-    }
-
-}
-
-
 
 public class Main_0612 {
     public static void main(String[] args) {
-        Dog dog = new Dog("쭈", "댕댕이");
+        Animal dog = new Dog("쭈", "댕댕이");
         dog.printName();
-
-        Vehicle ve = new Vehicle("train");
-        Car car = new Car("BMW", "Beetle");
-
-        ve.display();
-        car.display();
     }
 }
