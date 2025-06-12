56458fe90101abd456a841e7e67058e599832322
pikach-u <ziwon05@naver.com>
2025-06-12 09:44:13 +0900

    0611 계산기만들기 2

--- src/Main_0611.java
+++ src/Main_0611.java
@@ -7,56 +7,64 @@ public class Main_0611 {
 
         Scanner scanner = new Scanner(System.in);
 
-        //상수로 사칙연산 정의
+        //상수로 연산자 정의
         final String PLUS = "+";
         final String MINUS = "-";
         final String MULTIPLY = "*";
         final String DIVISION = "/";
+        final String END = "#";
 
-        double a = 0;    //왼쪽 피연산자
-        double b = 0;    //오른쪽 피연산자
+        double num1 = 0;    //왼쪽 피연산자
+        double num2 = 0;    //오른쪽 피연산자
         String op;          //연산자
         double answer = 0;  //답
 
 
         System.out.println("========계산기=========");
-        System.out.println("두 수를 입력해주세요.");
-        System.out.println("======================");
-
-        //계산할 숫자 입력
-        System.out.println("첫 번째 숫자: ");
-        a = scanner.nextDouble();
-
-        System.out.println("두 번째 숫자: ");
-        b = scanner.nextDouble();
-
-        System.out.println("1. + 2. - 3. * 4. /");
 
         while (true) {
+            System.out.println("두 수를 입력해주세요.");
+            System.out.println("======================");
 
+            //계산할 숫자 입력
+            System.out.println("첫 번째 숫자: ");
+            num1 = scanner.nextDouble();
+
+            System.out.println("두 번째 숫자: ");
+            num2 = scanner.nextDouble();
+
+            //연산자 입력
+            System.out.println("1. + 2. - 3. * 4. / (종료: #)");
             op = scanner.next();
 
             switch (op) {
                 case PLUS:
-                    answer = a + b;
+                    answer = num1 + num2;
                     break;
                 case MINUS:
-                    answer = a - b;
+                    answer = num1 - num2;
                     break;
                 case MULTIPLY:
-                    answer = a * b;
+                    answer = num1 * num2;
                     break;
                 case DIVISION:
-                    answer = a / b;
+                    if (num2 != 0) {
+                        answer = num1 / num2;
+                    } else {
+                        System.out.println("0으로 나눌 수 없습니다.");
+                        continue;
+                    }
                     break;
+                case END:
+                    return;
                 default:
                     System.out.println("다시 입력해주세요");
                     continue;
             }
-            break;
-        }
 
-        System.out.printf("답은 %.2f입니다.\n", answer);
+            System.out.printf("답은 %.2f입니다.\n", answer);
+
+        }
 
     }
 }
