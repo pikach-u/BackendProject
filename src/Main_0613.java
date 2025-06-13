812b14cc7d0150b8adfd994200633b5a79a8971f
pikach-u <ziwon05@naver.com>
2025-06-13 17:22:21 +0900

    0613 CPU

--- src/Main_0613.java
+++ src/Main_0613.java
@@ -1,26 +1,45 @@
-//0613 abstract-classes
+//0613 CPU
 
-abstract class Animall{
-    abstract void makeSound();
+class CPU {
+    public int register = 0;
+    public int[] memory = {5, 10, 0};
 
-    public void sleep() {
-        System.out.println("Zzz...");
+    private static final int LOAD = 1;
+    private static final int SAVE = 2;
+    private static final int ADD = 3;
+    private static final int SUB = 4;
+    private static final int HALT = 5;
+
+    private int[][] program = {
+            {LOAD, 0},    // register = memory[0]
+            {ADD, 1},    // register = register + memory[1]
+            {SAVE, 2},    // memory[2] = register
+            {HALT, 0}     // stop program
+
+    };
+
+    public void executeProgram() {
+        int pc = 0;
+        while(true){
+            int opcode = program[pc][0];
+            int operand = program[pc][1];
+
+            switch(opcode){
+                case LOAD:
+                    register = memory[operand];
+                    System.out.println("MOV: Loading memory[" + operand + "] (" + memory[0] + ") into register");
+                    break;
+                case SAVE:
+                    memory[operand] = register;
+                    System.out.println("SAVE: Saving memory[" + operand + "] (" + memory[0] + ") from register.");
+                    break;
+            }
+        }
     }
 }
-
-class Dogg extends Animall {
-    @Override
-    void makeSound() {
-        System.out.println("멍멍!");
-    }
-}
-
 
 public class Main_0613 {
     public static void main(String[] args) {
-        Animall dog = new Dogg();
 
-        dog.makeSound();
-        dog.sleep();
     }
 }
