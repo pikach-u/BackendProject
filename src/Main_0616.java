3eb1b5d463538f4eb1f3e4fa436a37a325b6e162
pikach-u <ziwon05@naver.com>
2025-06-16 14:05:48 +0900

    0616 OuterInMemory

--- src/Main_0616.java
+++ src/Main_0616.java
@@ -1,38 +1,23 @@
-//0616 inner class
+//0616 OuterInMemory
 
-public class Main_0616 {    //Button
+public class Main_0616 {    //Outter
+    private String data = "Outer data";
 
-    interface OnClickListener{      //interface
-        void onClick();     //메서드명만 선언
-    }
-
-    private OnClickListener listener;   //private 객체
-
-    public void setOnClickListener(OnClickListener listener) {
-        this.listener = listener;   //setter
-    }
-
-    public void click() {
-        if(listener != null) {
-            listener.onClick();
+    class Inner{
+        void printData(){
+            System.out.println(data);
         }
     }
 
-    private class ClickHandler implements OnClickListener { //private class
-        @Override
-        public void onClick() { //interface의 onClick 정의
-            System.out.println("Button was clicked!");
-        }
-    }
-
-    public void simulate() {
-        setOnClickListener(new ClickHandler()); //새로운 객체를 인자로 전달
-        click();    //click() 실행 -> .onClick() 실행
+    public Inner createInner(){
+        return new Inner();
     }
 
     public static void main(String[] args) {
-        Main_0616 button = new Main_0616();
-        button.simulate();
+        Main_0616 outer = new Main_0616();
+        Main_0616.Inner inner = outer.createInner();
+
+        inner.printData();
 
     }
 }
