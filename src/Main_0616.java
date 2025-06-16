e17839297dab79d68b6d93b81770b01546bbea6f
pikach-u <ziwon05@naver.com>
2025-06-16 14:21:48 +0900

    0616 NullPointerException

--- src/Main_0616.java
+++ src/Main_0616.java
@@ -1,23 +1,31 @@
-//0616 OuterInMemory
+//0616 NullPointerException
 
 public class Main_0616 {    //Outter
     private String data = "Outer data";
 
-    class Inner{
-        void printData(){
+    class Inner {
+        void printData() {
             System.out.println(data);
         }
     }
 
-    public Inner createInner(){
+    public Inner createInner() {
         return new Inner();
     }
 
     public static void main(String[] args) {
         Main_0616 outer = new Main_0616();
+        //Outer.this
         Main_0616.Inner inner = outer.createInner();
 
         inner.printData();
 
+        outer = null;
+
+        inner.printData();
+
+        inner = null;
+
+        System.gc();
     }
 }
