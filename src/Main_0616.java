2e9c13bed146e6fcc05b7f78c67a8daae7cc3949
pikach-u <ziwon05@naver.com>
2025-06-16 13:46:31 +0900

    0616 inner class_listener pattern

--- src/Main_0616.java
+++ src/Main_0616.java
@@ -1,23 +1,38 @@
 //0616 inner class
 
-class Outer {
+public class Main_0616 {    //Button
 
-    String outerMessage = "Hello from Outer";
+    interface OnClickListener{      //interface
+        void onClick();     //메서드명만 선언
+    }
 
-    private class Inner {   //오류!
-        void showMessage() {
-            System.out.println(outerMessage);
+    private OnClickListener listener;   //private 객체
+
+    public void setOnClickListener(OnClickListener listener) {
+        this.listener = listener;   //setter
+    }
+
+    public void click() {
+        if(listener != null) {
+            listener.onClick();
         }
     }
-}
 
-public class Main_0616 {
+    private class ClickHandler implements OnClickListener { //private class
+        @Override
+        public void onClick() { //interface의 onClick 정의
+            System.out.println("Button was clicked!");
+        }
+    }
+
+    public void simulate() {
+        setOnClickListener(new ClickHandler()); //새로운 객체를 인자로 전달
+        click();    //click() 실행 -> .onClick() 실행
+    }
 
     public static void main(String[] args) {
-        Outer outer = new Outer();
-        Outer.Inner inner = outer.new Inner();   //private은 접근x!!
-
-        inner.showMessage();
+        Main_0616 button = new Main_0616();
+        button.simulate();
 
     }
 }
