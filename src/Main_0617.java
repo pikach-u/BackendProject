d34d756666bad39e73c3f41a49e92c79f84b9408
pikach-u <ziwon05@naver.com>
2025-06-17 14:18:18 +0900

    0617 try+throws

--- src/Main_0617.java
+++ src/Main_0617.java
@@ -2,11 +2,25 @@
 
 public class Main_0617 {
     public static void main(String[] args) {
-        String input = "1 0 0";
-        String[] tokens = input.split(" ");
+        String input = "100";
 
-        for(String token: tokens){
-            System.out.println(token);
+        try {
+            boolean result = validateTokenCount(input);
+
+            if (result) {
+                System.out.println("two");
+            }
+        } catch (NumberFormatException e) {
+            System.out.println(e.getMessage());
         }
+
+    }
+
+    public static boolean validateTokenCount(String tokens) throws NumberFormatException {
+        if (tokens.length() != 2) {
+            throw new NumberFormatException("Not 2");
+        }
+
+        return true;
     }
 }
