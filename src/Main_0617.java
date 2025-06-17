b3410c3dd5881f36e85075cafa9d0267dbb87796
pikach-u <ziwon05@naver.com>
2025-06-17 11:02:52 +0900

    0617 try-catch

--- src/Main_0617.java
+++ src/Main_0617.java
@@ -1,17 +1,15 @@
-//0617 Method reference
-
-import java.lang.reflect.Array;
-import java.util.Arrays;
-
+//0617 try-catch
 
 public class Main_0617 {
     public static void main(String[] args) {
-        String[] name = {"홍성현", "김백기", "강윤정", "고종우"};
-        for(String n : name){
-            System.out.println(n);
+        int a = 10;
+        int b = 0;
+        try {
+            int result = a / b;
+            System.out.println("Result: " + result);
+        } catch (ArithmeticException e) {
+            System.out.println(e.getMessage());
+            System.out.println("Error: Division by zero");
         }
-        Arrays.stream(name).forEach(nn -> System.out.println(nn));
-        Arrays.stream(name).forEach(System.out::println);
-
     }
 }
