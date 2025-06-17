6a8fbbcd9862f845e39a2f94cba22af3b35b2c7c
pikach-u <ziwon05@naver.com>
2025-06-17 13:06:47 +0900

    0617 Parse

--- src/Main_0617.java
+++ src/Main_0617.java
@@ -1,20 +1,14 @@
-//0617 Lambda can be replaced with method reference
-
-class Util {
-    public static void sayHello(String name) {
-        System.out.println("Hello, " + name);
-    }
-}
-
-@FunctionalInterface
-interface A{
-    public void say(String name);
-}
+//0617 Parse
 
 public class Main_0617 {
     public static void main(String[] args) {
-        // A a = (name) -> Util.sayHello(name); //Lambda
-        A a = Util::sayHello;   //Method Reference
-        a.say("Hachiware");
+        String input = "123a";
+
+        try {
+            int number = Integer.parseInt(input);
+            System.out.println(number);
+        } catch (NumberFormatException e) {
+            System.out.println("숫자 포맷이 아닙니다.");
+        }
     }
 }
