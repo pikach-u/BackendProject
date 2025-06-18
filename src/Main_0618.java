4547e5a0d700e601ad599b95ab59b183943cc47c
pikach-u <ziwon05@naver.com>
2025-06-18 14:42:41 +0900

    0618 Map HashMap

--- src/Main_0618.java
+++ src/Main_0618.java
@@ -1,17 +1,27 @@
-//0618 Set
+//0618 Map
 
 import java.util.*;
 
 public class Main_0618 {
     public static void main(String[] args) {
+        Map<String, Integer> fruitPrices = new HashMap<>();
+        fruitPrices.put("사과", 17980);
+        fruitPrices.put("배", 10320);
+        fruitPrices.put("수박", 14900);
+        fruitPrices.put("멜론", 10630);
 
-        Set<String> fruits = new TreeSet<>();
+        for (Map.Entry<String, Integer> entry : fruitPrices.entrySet()) {
+            System.out.println(entry.getKey() + " costs " + entry.getValue() + "원"); //값 꺼내기
+        }
 
-        fruits.add("Apple");
-        fruits.add("Banana");
-        fruits.add("Kiwi");
+        double cost = 0;
 
-        for (String fruit : fruits) //출력 순서가 O
-            System.out.println(fruit);
+        for (int fruitPrice : fruitPrices.values()) {
+            cost += fruitPrice;
+            // System.out.println(fruitPrice);
+        }
+
+        System.out.println(cost / fruitPrices.size());
+
     }
 }
