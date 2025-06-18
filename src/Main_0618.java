fd21fedf59e7ab873d95bb27c9bbb24d7b2fe416
pikach-u <ziwon05@naver.com>
2025-06-18 10:45:34 +0900

    0618 List subList

--- src/Main_0618.java
+++ src/Main_0618.java
@@ -14,16 +14,13 @@ public class Main_0618 {
         fruits.add("Potato");
         fruits.add("Dragonfruit");
 
-        int index = fruits.indexOf("Banana");
-        fruits.set(1, "Orange"); // 1번 인덱스 값을 "Orange"로 수정
-        System.out.println(fruits.get(1));
-        List<String> sliced = fruits.subList(0, 2); // 0번부터 1번까지 추출 (2는 미포함)
+        List<String> subFruits = fruits.subList(2, 4);
 
-//        int index = fruits.indexOf("Banana");
-//        System.out.println(index);
-
-        for(String fruit : fruits)
+        for (String fruit : fruits)
             System.out.println(fruit);
+
+        for (String subFruit : subFruits)
+            System.out.println(subFruit);
 
 
     }
