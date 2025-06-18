948e0560b50d9ed463929066a776f9a70ccf64af
pikach-u <ziwon05@naver.com>
2025-06-18 10:25:58 +0900

    0618 List remove, clear

--- src/Main_0618.java
+++ src/Main_0618.java
@@ -6,20 +6,24 @@ public class Main_0618 {
     public static void main(String[] args) {
 
         List<String> fruits = new ArrayList<String>();
-        fruits.add("Apple");
-        fruits.add("Banana");
-        fruits.add("Cherry");
-        fruits.add("Dragonfruit");
         fruits.add("Mango");
+        fruits.add("Apple");
+        fruits.add("Tomato");
+        fruits.add("Cherry");
+        fruits.add("Banana");
+        fruits.add("Potato");
+        fruits.add("Dragonfruit");
 
-        if(fruits.contains("Kiwi")){
-            System.out.println("Kiwi is contained");
-        }
-        else{
-            System.out.println("Kiwi is not contained");
-        }
+        fruits.remove("Tomato"); // 값으로 삭제
+        fruits.remove(5);         // 인덱스로 삭제 (0번째 요소 삭제
 
         for(String fruit : fruits)
             System.out.println(fruit);
+
+        System.out.println(fruits.isEmpty());
+
+        fruits.clear();
+
+        System.out.println(fruits.isEmpty());
     }
 }
