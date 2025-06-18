2d257c72f05e52b6fe5cd8853ff1a1c96f7fb6d9
pikach-u <ziwon05@naver.com>
2025-06-18 10:37:04 +0900

    0618 List set,get

--- src/Main_0618.java
+++ src/Main_0618.java
@@ -14,16 +14,17 @@ public class Main_0618 {
         fruits.add("Potato");
         fruits.add("Dragonfruit");
 
-        fruits.remove("Tomato"); // 값으로 삭제
-        fruits.remove(5);         // 인덱스로 삭제 (0번째 요소 삭제
+        int index = fruits.indexOf("Banana");
+        fruits.set(1, "Orange"); // 1번 인덱스 값을 "Orange"로 수정
+        System.out.println(fruits.get(1));
+        List<String> sliced = fruits.subList(0, 2); // 0번부터 1번까지 추출 (2는 미포함)
+
+//        int index = fruits.indexOf("Banana");
+//        System.out.println(index);
 
         for(String fruit : fruits)
             System.out.println(fruit);
 
-        System.out.println(fruits.isEmpty());
 
-        fruits.clear();
-
-        System.out.println(fruits.isEmpty());
     }
 }
