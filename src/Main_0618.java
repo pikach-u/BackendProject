78c482d8f8ec46e2f519a713e9adfbe8efce5a2c
pikach-u <ziwon05@naver.com>
2025-06-18 10:04:04 +0900

    0618 List sort

--- src/Main_0618.java
+++ src/Main_0618.java
@@ -1,8 +1,6 @@
 //0618 List
 
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
+import java.util.*;
 
 public class Main_0618 {
     public static void main(String[] args) {
@@ -14,16 +12,7 @@ public class Main_0618 {
         fruits.add("Dragonfruit");
         fruits.add("Mango");
 
-//        for(int i = 0; i< fruits.size(); i++)
-//            System.out.println(fruits.get(i));
-
-
-//        Iterator<String> it = fruits.iterator();
-//        while (it.hasNext()) {
-//            String item = it.next();
-//            System.out.println(item);
-//        }     // 아래와 똑같이 동작
-
+        Collections.sort(fruits);
 
         for(String fruit : fruits)
             System.out.println(fruit);
