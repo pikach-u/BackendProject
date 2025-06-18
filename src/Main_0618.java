d5928e5f6cee33bfde84a6534cf8ddaf3397ac4b
pikach-u <ziwon05@naver.com>
2025-06-18 11:21:02 +0900

    0618 List sum, size

--- src/Main_0618.java
+++ src/Main_0618.java
@@ -5,23 +5,23 @@ import java.util.*;
 public class Main_0618 {
     public static void main(String[] args) {
 
-        List<String> fruits = new ArrayList<String>();
-        fruits.add("Mango");
+        List<Integer> numbers = new ArrayList<>();
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+        int sum = 0;
+        for (int num : numbers) {
+            sum += num;
+        }
+        System.out.println(sum);
+
+        List<String> fruits = new ArrayList<>();
         fruits.add("Apple");
-        fruits.add("Tomato");
-        fruits.add("Cherry");
         fruits.add("Banana");
-        fruits.add("Potato");
-        fruits.add("Dragonfruit");
 
-        List<String> subFruits = fruits.subList(2, 4);
-
-        for (String fruit : fruits)
-            System.out.println(fruit);
-
-        for (String subFruit : subFruits)
-            System.out.println(subFruit);
-
+        if (fruits.size() > 0) {
+            System.out.println("리스트에 항목이 있습니다.");
+        }
 
     }
 }
