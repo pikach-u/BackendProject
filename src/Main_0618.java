353b8a05a1dabe0ab6fa55ae218c001c572a9240
pikach-u <ziwon05@naver.com>
2025-06-18 13:45:06 +0900

    0618 set LinkedHashSet

--- src/Main_0618.java
+++ src/Main_0618.java
@@ -5,17 +5,13 @@ import java.util.*;
 public class Main_0618 {
     public static void main(String[] args) {
 
-        Set<String> fruits = new HashSet<>();
+        Set<String> fruits = new LinkedHashSet<>();
 
         fruits.add("Apple");
         fruits.add("Banana");
-        fruits.add("Apple");    //중복 X
-        fruits.add("banana");
-        fruits.add("apple");    //대소문자 구분
         fruits.add("Kiwi");
-        fruits.add("kiwi");
 
         for (String fruit : fruits) //출력 순서가 O
-            System.out.println(fruit.hashCode());
+            System.out.println(fruit);
     }
 }
