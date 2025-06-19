78bd38debf293f2300e27e7893f303fe9b0c7931
pikach-u <ziwon05@naver.com>
2025-06-19 14:32:20 +0900

    0619 Generic WildCard

--- src/Main_0619.java
+++ src/Main_0619.java
@@ -1,46 +1,16 @@
 //0619 Generic
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
-
-class Box {
-    public static <T> void displayArray(List<T> array) {
-        for (T element : array) {
-            System.out.println(element);
-        }
-    }
-
-}
 
 public class Main_0619 {
     public static void main(String[] args) {
-//        Integer[] numbers = {1, 2, 3, 4, 5};
-//        String[] words = {"apple", "banana", "cherry"};
-//        Box.displayArray(numbers);
+//        Object[] arr = new Integer[3];
+//        arr[1] = "Hello";   //ArrayStoreException
 
-        List<Integer> numbers = List.of(1, 2, 3, 4, 5);     //Immutable Object
-        Box.displayArray(numbers);
+//        List<Object> arr = new ArrayList<Integer>();    // X, 제네릭은 불공변
+        List<? extends Number> arr = new ArrayList<Integer>();  //Wild Card를 통해 공변성 부여
 
-//        numbers.add(6);     //X
-//        numbers.remove(3);  //X
-//        Box.displayArray(numbers);
-
-        List<Integer> number = new ArrayList<>(List.of(1, 2, 3, 4, 5));     //mutable Object
-        number.add(6);
-        Box.displayArray(number);
-
-
-//        String[] words = {"apple", "banana", "cherry"};
-        List<String> words = new ArrayList<>();
-        words.add("apple");
-        words.add("banana");
-        words.add("cherry");
-
-        List<String> word = new ArrayList<>(Arrays.asList("apple", "banana", "cherry"));
-
-        Box.displayArray(words);
-        Box.displayArray(word);
 
     }
 }
