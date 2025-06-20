e479404e58de54c262575dea4974926d37bb4b44
pikach-u <ziwon05@naver.com>
2025-06-20 13:07:30 +0900

    0620 Thread

--- src/Main_0620.java
+++ src/Main_0620.java
@@ -1,18 +1,26 @@
-//0620 Stream
+//0620 Thread
 
-import java.util.Arrays;
-import java.util.List;
+import java.util.Timer;
+
+class MyThread extends Thread {      //Thread 상속
+    @Override
+    public void run() {
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("My Thread: " + i);
+            try {
+                Thread.sleep(5000);     // 예외처리 필수
+            } catch (InterruptedException e) {  //.sleep()이 실행되는 동안 외부에서 작업이 들어올 수 있음
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+}
 
 public class Main_0620 {
     public static void main(String[] args) {
-        List<Double> values = Arrays.asList(10.0, 20.0, 30.0);
+        MyThread thread = new MyThread();
+        thread.start();
 
-        double avg = values.stream()
-                .mapToDouble(Double::doubleValue)
-                .average()
-                .orElse(0.0);   //안전장치
-
-        System.out.println(avg);
-
+        thread.run();   // run을 직접 호출하면 멀티스레딩 X
     }
 }
