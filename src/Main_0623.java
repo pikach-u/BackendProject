56c1bafb632e965ce772beaa296eb2afd3a96978
pikach-u <ziwon05@naver.com>
2025-06-23 09:18:32 +0900

    0620 test Easy1

---
+++ src/Main_0623.java
@@ -0,0 +1,10 @@
+public class Main_0623 {
+    public static int getFullPageCount(int itemsCount, int itemsPerPage) {
+        return itemsCount / itemsPerPage;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(getFullPageCount(10, 5));   // 2
+        System.out.println(getFullPageCount(7, 2));    // 3
+    }
+}
