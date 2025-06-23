d3222691f7d7029eac592ec4d9f7896452be8ca5
pikach-u <ziwon05@naver.com>
2025-06-23 14:10:53 +0900

    0623 Annotation_Reflection SuppressWarnings

--- src/Main_0623.java
+++ src/Main_0623.java
@@ -1,34 +1,14 @@
 //0623 Annotation_Reflection
 
-import java.awt.*;
-
-class Calculatorr {
-    /** javadoc
-     *
-     * @deprecated Use {@link #addNum(int, int)} instead.
-     */
-    @Deprecated     //경고. 더이상 사용하지 않는 메서드나 클래스
-    public int add(int a, int b) {
-        return a + b;
-    }
-
-    /**
-     * 두 수를 더합니다.
-     *
-     * @param a 첫 번째 정수
-     * @param b 두 번째 정수
-     * @return 두 수의 합
-     */
-
-    public int addNum(int a, int b) {
-        return a + b;
-    }
-}
-
 public class Main_0623 {
+    @SuppressWarnings("rawtypes")   //경고 무시
     public static void main(String[] args) {
-        Calculatorr calculatorr = new Calculatorr();
-        System.out.println(calculatorr.add(1, 2));
-        System.out.println(calculatorr.addNum(2, 4));
+        java.util.List list = new java.util.ArrayList<>();
+        list.add("Hello");
+        list.add("World");
+
+        for(Object obj : list){
+            System.out.println(obj);
+        }
     }
 }
