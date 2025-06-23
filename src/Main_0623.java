772397f14cca4420612e3b23babdb8256fceaab0
pikach-u <ziwon05@naver.com>
2025-06-23 13:48:05 +0900

    0623 Annotation_Reflection javadoc

--- src/Main_0623.java
+++ src/Main_0623.java
@@ -1,21 +1,34 @@
 //0623 Annotation_Reflection
 
-class Animall {
-    public void sound() {
-        System.out.println("Animal makes a sound.");
-    }
-}
+import java.awt.*;
 
-class Dogg extends Animall {
-    @Override
-    public void sound() {
-        System.out.println("Dog barks.");
+class Calculatorr {
+    /** javadoc
+     *
+     * @deprecated Use {@link #addNum(int, int)} instead.
+     */
+    @Deprecated     //경고. 더이상 사용하지 않는 메서드나 클래스
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    /**
+     * 두 수를 더합니다.
+     *
+     * @param a 첫 번째 정수
+     * @param b 두 번째 정수
+     * @return 두 수의 합
+     */
+
+    public int addNum(int a, int b) {
+        return a + b;
     }
 }
 
 public class Main_0623 {
     public static void main(String[] args) {
-        Animall dog = new Dogg();
-        dog.sound();
+        Calculatorr calculatorr = new Calculatorr();
+        System.out.println(calculatorr.add(1, 2));
+        System.out.println(calculatorr.addNum(2, 4));
     }
 }
