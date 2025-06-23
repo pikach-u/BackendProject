ed3bd5d23225bebc821a9b33b8dfd956a7379bd6
pikach-u <ziwon05@naver.com>
2025-06-23 14:25:45 +0900

    0623 Annotation_Reflection FunctionalInterface

--- src/Main_0623.java
+++ src/Main_0623.java
@@ -1,14 +1,13 @@
 //0623 Annotation_Reflection
 
-public class Main_0623 {
-    @SuppressWarnings("rawtypes")   //경고 무시
-    public static void main(String[] args) {
-        java.util.List list = new java.util.ArrayList<>();
-        list.add("Hello");
-        list.add("World");
+@FunctionalInterface
+interface Greeting{
+    void sayHello(String name);
+}
 
-        for(Object obj : list){
-            System.out.println(obj);
-        }
+public class Main_0623 {
+    public static void main(String[] args) {
+        Greeting greeting = (name) -> System.out.println("Hello" + name + "!");
+        greeting.sayHello(" Alice");
     }
 }
