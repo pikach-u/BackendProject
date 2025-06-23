92e8261f1999681ed1080bcfd525d71681a6a7d8
pikach-u <ziwon05@naver.com>
2025-06-23 09:28:58 +0900

    0620 test Easy2

--- src/Main_0623.java
+++ src/Main_0623.java
@@ -1,10 +1,14 @@
 public class Main_0623 {
-    public static int getFullPageCount(int itemsCount, int itemsPerPage) {
-        return itemsCount / itemsPerPage;
+    public static int getProductAbs(int x, int y) {
+        int result = x * y;
+
+        if (result < 0) {
+            return Math.abs(result);
+        } else return result;
     }
 
     public static void main(String[] args) {
-        System.out.println(getFullPageCount(10, 5));   // 2
-        System.out.println(getFullPageCount(7, 2));    // 3
+        System.out.println(getProductAbs(10, 20));      // 200
+        System.out.println(getProductAbs(20, -40));     // 800
     }
 }
