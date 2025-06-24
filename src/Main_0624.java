dc8da7279dbb8c9f7f8c515fe00c944f2f039593
pikach-u <ziwon05@naver.com>
2025-06-24 13:41:42 +0900

    250624 File-IO FileChannel

--- src/Main_0624.java
+++ src/Main_0624.java
@@ -1,34 +1,33 @@
 //250624 File-IO
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.FileWriter;
 import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.util.Arrays;
 
 public class Main_0624 {
-    public static void writeFile(String filename,String content) {
-        try (FileWriter writer = new FileWriter(filename, true)) {
-            writer.write(content);
-        } catch (IOException e) {
+    public static void writeFile(String filename, String content){
+        Path filePath = Paths.get(filename);
+
+        try(FileChannel writeChannel = FileChannel.open(filePath, StandardOpenOption.CREATE,StandardOpenOption.WRITE)){
+            //파일 경로, 파일이 없다면 생성, 쓰기 모드
+            ByteBuffer buffer = ByteBuffer.allocate(1024);  //1kb 한번 옮기는 바구니
+            buffer.put(content.getBytes()); //그냥 String으로 넣으면 X
+            //System.out.println("Byte Content: " + Arrays.toString(content.getBytes()));
+            buffer.flip();
+            writeChannel.write(buffer);
+            System.out.println("파일을 성공적으로 작성했습니다.");
+
+        }catch(IOException e){
             e.printStackTrace();
         }
+
+        System.out.println(filePath);
     }
-
-    public static void readFile(String filename) {
-        try (BufferedReader reader = new BufferedReader(new FileReader(filename))) {
-            String line;
-
-            while ((line = reader.readLine()) != null) {
-                System.out.println(line);
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
     public static void main(String[] args) {
-        writeFile("lunch-menu.txt","\n중에 하나를 드셔보세요!");
-        System.out.println("파일 생성 완료");
-        readFile("lunch-menu.txt");
+        writeFile("dinner-menu.txt", "오늘의 저녁 메뉴는 뭘까요?");
     }
 }
