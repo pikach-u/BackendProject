de5f3b1dbfc9aa3cc7c2d04944bfee240d089d94
pikach-u <ziwon05@naver.com>
2025-06-24 13:07:43 +0900

    250624 File-IO

---
+++ src/Main_0624.java
@@ -0,0 +1,25 @@
+//250624 File-IO
+
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class Main_0624 {
+    public static void writeFile(String filename,String content) {
+        FileWriter writer = null;
+
+        try {
+            writer = new FileWriter(filename);
+            writer.write(content);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (writer != null) writer.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+    public static void main(String[] args) {
+    }
+}
