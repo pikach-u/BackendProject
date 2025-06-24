2e323934cfe33092d8dc9be81e95af3dc7818ec4
pikach-u <ziwon05@naver.com>
2025-06-24 13:14:15 +0900

    250624 File-IO readFile

--- src/Main_0624.java
+++ src/Main_0624.java
@@ -1,27 +1,34 @@
 //250624 File-IO
 
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 
 public class Main_0624 {
     public static void writeFile(String filename,String content) {
-        FileWriter writer = null;
-
-        try {
-            writer = new FileWriter(filename);
+        try (FileWriter writer = new FileWriter(filename, true)) {
             writer.write(content);
         } catch (IOException e) {
             e.printStackTrace();
-        } finally {
-            try {
-                if (writer != null) writer.close();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
         }
     }
+
+    public static void readFile(String filename) {
+        try (BufferedReader reader = new BufferedReader(new FileReader(filename))) {
+            String line;
+
+            while ((line = reader.readLine()) != null) {
+                System.out.println(line);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
     public static void main(String[] args) {
-        writeFile("lunch-menu.txt","오늘의 점심 메뉴는 무엇인가요?");
+        writeFile("lunch-menu.txt","\n중에 하나를 드셔보세요!");
         System.out.println("파일 생성 완료");
+        readFile("lunch-menu.txt");
     }
 }
